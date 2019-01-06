03b0e7a1ea072a5df597b667c1bbea8c29ae571d
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2019-01-06 20:57:45 -0300

    Change reducer structure from if statement to swtich

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,18 @@
 export const quotesReducer = (state = [], action) => {
-  if (action.type === 'ADD_QUOTE') {
-    return [...state, action.payload];
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.payload];
+    case 'EDIT_QUOTE':
+      return state.map(quote => quote.id === action.payload.id ? action.payload : quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.paylaod);
+    case 'FETCH_IMAGE':
+      return state
+    case 'FETCH_RANDOM_IMAGE':
+      return state
+    case 'FETCH_RANDOM_QUOTE':
+      return state
+    default:
+      return state
   }
-
-  if (action.type === 'EDIT_QUOTE') {
-    return state.map(quote => quote.id === action.payload.id ? action.payload : quote);
-  }
-
-  if (action.type === 'REMOVE_Quote') {
-    return state.filter(quote => quote.id !== action.paylaod);
-  }
-  return state
 }
