48a71701fa1eb7e29a31890badb060ae8e6404e2
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2019-01-12 14:57:54 -0300

    Refactor the quoteReducer code

--- src/reducers/quotesReducer.js
+++ src/reducers/quotesReducer.js
@@ -3,21 +3,19 @@ export const initialState = {
   term: '',
   image: '',
   selectedQuote: {
-
   }
 }
 
 export const quotesReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_QUOTE':
-      return {...state, list: [...state.list, action.payload], image: '', term: ''}
+      return {...state, list: [ ...state.list, action.payload ], image: '', term: '' }
     case 'EDIT_QUOTE':
       return state.list.map(quote => quote.id === action.payload.id ? action.payload : quote);
     case 'REMOVE_QUOTE':
       return state.list.filter(quote => quote.id !== action.payload);
     case 'GET_QUOTE':
-      const selectedQuote = state.list.find(quote => quote.id === action.payload)
-      return {...state, selectedQuote};
+      return { ...state, selectedQuote: state.list.find(quote => quote.id === action.payload) };
     case 'SET_TERM':
       return { ...state, term: action.payload }
     case 'IMAGE_RECEIVED':
@@ -25,7 +23,7 @@ export const quotesReducer = (state = initialState, action) => {
     case 'REMOVE_IMAGE':
       return { ...state, image: '' }
     case 'RANDOM_QUOTE_RECEIVED':
-      return { ...state, list: [...state.list, action.payload] }
+      return { ...state, list: [ ...state.list, action.payload ] }
     default:
       return state
   }
